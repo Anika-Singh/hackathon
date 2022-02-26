eb35fb6cab25fde2b23c8f809d8a5b57ca404d2a
Anika-Singh <anikasingh2001@gmail.com>
2022-02-26 03:23:17 -0600

    simplified

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,20 +1,19 @@
-const router = require('express').Router();
-let User = require('../models/user.model');
+const mongoose = require("mongoose");
 
-router.route('/').get((req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
 });
 
-router.route('/add').post((req, res) => {
-  const username = req.body.username;
-
-  const newUser = new User({username});
-
-  newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
-module.exports = router;
+const UserModel = mongoose.model("users", UserSchema);
+module.exports = UserModel;
--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-// import { Router } from 'react-router';
-
-import Navbar from "./components/navbar.component"
-import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
+import "./App.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data);
+    });
+  }, []);
+
+  const createUser = () => {
+    Axios.post("http://localhost:3001/createUser", {
+      name,
+      age,
+      username,
+    }).then((response) => {
+      setListOfUsers([
+        ...listOfUsers,
+        {
+          name,
+          age,
+          username,
+        },
+      ]);
+    });
+  };
+
   return (
-    <Router>
-      <div className="container">
-      <Navbar />
-      {/* <Route path="/" component={ExercisesList}/> */}
-      {/* <Route path="/edit/:id" component={EditExercise} /> */}
-      {/* <Route path="/create" component={CreateExercise} /> */}
-      {/* <Route path="/user" component={CreateUser} /> */}
+    <div className="App">
+      <div className="usersDisplay">
+        {listOfUsers.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>Username: {user.username}</h1>
+            </div>
+          );
+        })}
       </div>
-    </Router>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <button onClick={createUser}> Create User </button>
+      </div>
+    </div>
   );
 }
 
