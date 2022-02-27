b89def55fe251048a09c43a431b6271971dbceb9
alisonjin <alison.q.jin@gmail.com>
2022-02-26 19:04:01 -0600

    services -> values section

--- src/pages/home.js
+++ src/pages/home.js
@@ -14,39 +14,39 @@ class Home extends Component {
                     <div class="container">
                         <div class="masthead-subheading">Welcome To Haven!</div>
                         <div class="masthead-heading text-uppercase">Your safehouse away from home.</div>
-                        <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+                        <a class="btn btn-primary btn-xl text-uppercase" href="#values">Tell Me More</a>
                     </div>
 
-                    <section class="page-section" id="services">
+                    <section class="page-section" id="values">
                         <div class="container">
                             <div class="text-center">
-                                <h2 class="section-heading text-uppercase">Services</h2>
-                                <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                                <h2 class="section-heading text-uppercase">Values</h2>
+                                {/* <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> */}
                             </div>
                             <div class="row text-center">
                                 <div class="col-md-4">
                                     <span class="fa-stack fa-4x">
                                         <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                                        <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                                        <i class="fas fa-child fa-stack-1x fa-inverse"></i>
                                     </span>
-                                    <h4 class="my-3">E-Commerce</h4>
-                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                                    <h4 class="my-3">Community</h4>
+                                    <p class="text-muted">We strive to create a safe community for females to find housing in new areas. A trusted community is a must to cultivate a system of safehouses that are reliable to find your new haven away from home.</p>
                                 </div>
                                 <div class="col-md-4">
                                     <span class="fa-stack fa-4x">
                                         <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                                        <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                                        <i class="fas fa-bed fa-stack-1x fa-inverse"></i>
                                     </span>
-                                    <h4 class="my-3">Responsive Design</h4>
-                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                                    <h4 class="my-3">Comfort</h4>
+                                    <p class="text-muted">Your home away from home will situated only in housing that we and our users love living in. Comfort is key to making your stay an enjoyable experience.</p>
                                 </div>
                                 <div class="col-md-4">
                                     <span class="fa-stack fa-4x">
                                         <i class="fas fa-circle fa-stack-2x text-primary"></i>
                                         <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
                                     </span>
-                                    <h4 class="my-3">Web Security</h4>
-                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                                    <h4 class="my-3">Safety</h4>
+                                    <p class="text-muted">Safety is our #1 priority. Your haven will be rated based off data on the neighborhood and more importantly, how previous residents felt. We want to give you total peace of mind while living in a new area.</p>
                                 </div>
                             </div>
                         </div>
@@ -259,7 +259,7 @@ class Home extends Component {
                         </div>
                     </div>
                 </section>
-                {/* <!-- Clients--> */}
+                {/* <!-- Clients-->
                 <div class="py-5">
                     <div class="container">
                         <div class="row align-items-center">
@@ -277,7 +277,7 @@ class Home extends Component {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
                 <Signin />
                {/* <!-- Sign In--> */}
                 {/* <!-- Footer--> */}
